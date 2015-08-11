c06d13327a93db672e95073fbd05200c1bdcaaff
cwade <courtneywade@gmail.com>
2015-08-11 15:15:26 -0400

    Added d3/html frequency report

---
+++ output/d3/helpers.js
@@ -0,0 +1,31 @@
+function wrap(text, length) {
+  ind = 0
+  text.each(function() {
+    if (length instanceof Array) {
+      width = length[ind]-25
+    } else {
+      width = length
+    }
+    var text = d3.select(this),
+        words = text.text().split(/\s+/).reverse(),
+        word,
+        line = [],
+        lineNumber = 0,
+        lineHeight = 1.1, // ems
+        y = text.attr("y"),
+        dy = parseFloat(text.attr("dy")),
+        tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
+    while (word = words.pop()) {
+      line.push(word);
+      tspan.text(line.join(" "));
+      if (tspan.node().getComputedTextLength() > width) {
+        line.pop();
+        tspan.text(line.join(" "));
+        line = [word];
+        tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+      }
+    }
+    text.attr("numlines", lineNumber+1).attr("class", "wrapped_text")
+    ind += 1
+  });
+}
---
+++ surveyhelper/frequency_report.py
@@ -0,0 +1,43 @@
+"""
+FrequencyReport
+--------
+Provides utilities for producing a survey frequency report.
+"""
+
+from jinja2 import Environment, FileSystemLoader
+import yaml
+from unidecode import unidecode
+
+class FrequencyReport:
+
+    def __init__(self, response_set, config_file):
+        with open(config_file, 'r') as ymlfile:
+            cfg = yaml.load(ymlfile)
+
+        self.template_dir = cfg['output']['template_dir']
+        self.freq_template = cfg['output']['template_file']
+        self.report_file = cfg['output']['report_file']
+        self.cut_var = cfg['analysis']['cut_variable']
+        self.report_title = cfg['report_data']['title']
+        self.response_set = response_set
+
+    def create_report(self):
+        env = Environment(loader=FileSystemLoader(self.template_dir),
+                  extensions=['jinja2.ext.with_'])
+        template = env.get_template(self.freq_template)
+        outfile = open(self.report_file, 'w+')
+        questions = []
+        for q in self.response_set.matched_questions:
+            questions.append((
+                            q.text,
+                            q.freq_table_to_json(self.response_set.data),
+                            [q.freq_table_to_json(self.response_set.data)],
+                            q.questions_to_json(),
+                            ['all'],
+                            q.graph_type
+                            ))
+        t = template.render(count=len(self.response_set.data),
+                                      survey_title=self.report_title,
+                                      questions=questions)
+        outfile.write(unidecode(t))
+
